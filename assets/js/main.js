f9b29892fc0ed15b4d19759fb3db6289dd6c7244
Jaisson Santos <jaissonssantos@gmail.com>
2018-03-05 17:27:59 -0500

    update no site;

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,20 @@
 $(document).ready(function(){
 
-    //busca
+    //menu abre
     $('a#menu-aberto').on('click',function(event){
-        $(this).addClass('invisible');
-        $('#menu-fechado').removeClass('invisible');
-        $('.oac-opacidade-menu').removeClass('invisible');
+        $(this).addClass('hidden-xs-up');
+        $('#menu-fechado').removeClass('hidden-xs-up');
+        $('.m-ver-mais').removeClass('hidden-xs-up');
+        $('.oac-opacidade-menu').removeClass('hidden-xs-up');
+        return false;
+    });
+
+    //menu fecha
+    $('a#menu-fechado').on('click',function(event){
+        $(this).addClass('hidden-xs-up');
+        $('#menu-aberto').removeClass('hidden-xs-up');
+        $('.m-ver-mais').addClass('hidden-xs-up');
+        $('.oac-opacidade-menu').addClass('hidden-xs-up');
         return false;
     });
 
