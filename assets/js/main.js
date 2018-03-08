2341b5e4470d539efd07655749f61ea500991dcc
Jaisson Santos <jaissonssantos@gmail.com>
2018-03-08 11:05:16 -0500

    update no template;

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
     var $window = $(window);
     var header = $('.oac-topo');
     $window.scroll(function () {
-        if ($window.scrollTop() >= 255) {
+        if ($window.scrollTop() >= 20) {
             header.addClass('oac-topo-reduzido');
         } else {
             header.removeClass('oac-topo-reduzido');
@@ -25,6 +25,12 @@ $(document).ready(function(){
         $('#menu-aberto').removeClass('hidden-xs-up');
         $('.m-ver-mais').addClass('hidden-xs-up');
         $('.oac-opacidade-menu').addClass('hidden-xs-up');
+        return false;
+    });
+
+    $('a#sub').on('click',function(event){
+        var find = $(this).parent();
+        find.find('.sub-dropdown').toggle();
         return false;
     });
 
