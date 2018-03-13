b7004657569f71c2474e46a6bf163f91507be039
Jaisson Santos <jaissonssantos@gmail.com>
2018-03-13 12:04:03 -0500

    update;

--- template_html/oac-v1/index.php
+++ template_html/oac-v1/index.php
@@ -13,6 +13,59 @@
 			</div>
 		</div>
 	</div>
+
+	<div class="row"><!-- categoria(manchete) -->
+		<div class="col-sm-6">
+			<?php 
+             $categoria = get_cat_id('manchete');
+             query_posts('showposts=1&offset=0&order=DESC&cat='.$categoria);?>
+            <?php if (have_posts()): while (have_posts()) : the_post();?>
+			<article class="oac-destaque oac-destaque-esquerdo">
+				<a href="artigo.html">
+					<div class="legenda">
+						<span>Oportunidade</span>
+					</div>
+					<div class="sombra"></div>
+					<figure style="background-image: url('assets/images/destaque01.jpg');">
+					</figure>
+					<div class="titulo">
+						<h1>Detran-AC abre 20 vagas para contratação temporária de examinador de trânsito</h1>
+					</div>
+				</a>
+			</article>
+			<?php endwhile; else:?>
+            <?php endif;?>
+		</div>
+		<div class="col-sm-6">
+			<article class="oac-destaque oac-destaque-direito">
+				<a href="artigo.html">
+					<div class="legenda">
+						<span>Aponta Anatel</span>
+					</div>
+					<div class="sombra"></div>
+					<figure style="background-image: url('assets/images/destaque02.jpg');">
+					</figure>
+					<div class="titulo">
+						<h1>Mais de 21 mil linhas telefônicas são canceladas no Acre em um ano, aponta Anatel</h1>
+					</div>
+				</a>
+			</article>
+
+			<article class="oac-destaque oac-destaque-direito">
+				<a href="artigo.html">
+					<div class="legenda">
+						<span>Em Rio Branco</span>
+					</div>
+					<div class="sombra"></div>
+					<figure style="background-image: url('assets/images/destaque04.jpg');">
+					</figure>
+					<div class="titulo">
+						<h1>Prefeitura de Rio Branco sanciona lei que autoriza empréstimo de R$ 14,5 milhões para término de obras do shopping popular</h1>
+					</div>
+				</a>
+			</article>
+		</div>
+	</div>
         
 <?php get_footer(); ?>
 
